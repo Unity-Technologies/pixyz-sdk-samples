c18c384d44087953ef63dc3c095304936e9674f6
Bierwirth, Daniel <daniel.bierwirth@unity3d.com>
2024-12-17 10:54:22 +0100

    adding ImportOptimiseExport sample
    adding C# .net import, optimize, export console sample

--- csharp/Windows/ImportOptimiseExport/ImportOptimiseExport/Program.cs
+++ csharp/Windows/ImportOptimiseExport/ImportOptimiseExport/Program.cs
@@ -1,2 +1,146 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PiXYZDemo
+{
+    using UnityEngine.Pixyz.Algo;
+    using UnityEngine.Pixyz.Scene;
+    using UnityEngine.Pixyz.IO;
+    
+    using static System.Formats.Asn1.AsnWriter;
+    using System.Xml.Linq;
+    using UnityEngine.Pixyz.API;
+
+    class Program
+    {
+        static PiXYZAPI api = PiXYZAPI.Initialize();
+
+        static void OptimiseModel(uint root, string fileName)
+        {
+            var stats = PixyzUtils.GetStats(api, root);
+            var t0 = stats.Item1;
+            var n_triangles = stats.Item2;
+            var n_vertices = stats.Item3;
+            var n_parts = stats.Item4;
+
+            var occurrences = new OccurrenceList((int)root);
+
+            Console.WriteLine("Removing Holes...");
+            double diameter = 10;
+            api.Algo.RemoveHoles(occurrences, true, false, false, diameter, 0);
+
+            Console.WriteLine("Delete Patches...");
+            api.Algo.DeletePatches(occurrences, true);
+
+            Console.WriteLine("Decimating...");
+            api.Algo.Decimate(occurrences, 1, 0.1, 3, -1, false);
+
+            Console.WriteLine("Removing Hidden Geometries...");
+            api.Algo.RemoveOccludedGeometries(occurrences, SelectionLevel.Polygons, 1024, 16, 90, false, 1);
+
+            var newStats = PixyzUtils.GetStats(api, root);
+            var t1 = newStats.Item1;
+            PixyzUtils.PrintStats(fileName, t1 - t0, n_triangles, newStats.Item2, n_vertices, newStats.Item3, n_parts, newStats.Item4);
+        }
+
+        static void ConvertFile(string inputFile, string outputFolder, List<string> extensions, bool optimization, string orgId, string projId, string collectionPath, List<string> tags)
+        {
+            var modelFiles = new List<string>();
+            var fileName = Path.GetFileNameWithoutExtension(inputFile);
+
+            api.Core.SetLogFile(Path.Combine(outputFolder, fileName + ".log"));
+
+            var root = ImportModel(inputFile);
+
+            PrepareModel(root);
+
+            if (optimization)
+            {
+                OptimiseModel(root, fileName);
+            }
+
+            foreach (var extension in extensions)
+            {
+                Console.WriteLine($"Exporting {extension}...");
+                api.IO.ExportScene(Path.Combine(outputFolder, fileName + extension));
+                modelFiles.Add(Path.Combine(outputFolder, fileName + extension));
+            }
+        }
+
+        static uint ImportModel(string filepath)
+        {
+            Console.WriteLine($"Importing {filepath}...");
+            return api.IO.ImportScene(filepath);
+        }
+
+        static void PrepareModel(uint root)
+        {
+            var occurrences = new OccurrenceList((int)root);
+
+            Console.WriteLine("Calculating Tolerances... ");
+            double tolerance = Math.Min(MathUtils.AabbDiagLength(api.Scene.GetAABB(occurrences)) / 1000, 0.1);
+
+            Console.WriteLine("Repairing CAD... ");
+            api.Algo.RepairCAD(occurrences, tolerance, false);
+
+            Console.WriteLine("Repairing Meshes... ");
+            api.Algo.RepairMesh(occurrences, tolerance, true, false);
+
+            Console.WriteLine("Tessellating Meshes... ");
+            api.Algo.Tessellate(occurrences, tolerance, -1, -1);
+        }
+
+        static void ExportModel(string filepath, string extension, uint root)
+        {
+            var finalPath = Path.Combine(Path.GetDirectoryName(filepath), Path.GetFileNameWithoutExtension(filepath) + extension);
+            Console.WriteLine($"Exporting {finalPath}...");
+            api.IO.ExportScene(finalPath, root);
+        }
+
+        static void ConfigureLicenseAndTokens()
+        {
+            if (!api.Core.CheckLicense())
+            {
+                api.Core.ConfigureLicenseServer("lics-it-gcp-p01", 27005, true);
+                Console.WriteLine("Configured license server.");
+            }
+
+            Console.WriteLine("Adding available tokens.");
+            foreach (var token in api.Core.ListTokens().list)
+            {
+                try
+                {
+                    api.Core.NeedToken(token);
+                    Console.WriteLine($"Token {token} added successfully.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Failed to add token {token}: {e.Message}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            string modelFilePath = "../../../../../../../python/Windows/shared/shared_models/SkidLoaderSolidworks/Skid Loader ASSM 11-4-21.SLDASM";
+
+            ConfigureLicenseAndTokens();
+
+            if (api.Core.CheckLicense())
+            {
+                var root = ImportModel(modelFilePath);
+                PixyzUtils.SaveScreenshot(api, (int)root, Path.Combine(Path.GetDirectoryName(modelFilePath), "before.png"), Orientation.LEFT);
+                PrepareModel(root);
+                PixyzUtils.SaveScreenshot(api, (int)root, Path.Combine(Path.GetDirectoryName(modelFilePath), "after.png"), Orientation.FRONT);
+                PixyzUtils.ExtractHierarchyToJson(api, root, Path.Combine(Path.GetDirectoryName(modelFilePath), $"{Path.GetFileNameWithoutExtension(modelFilePath)}.json"));
+                ExportModel(modelFilePath, "_new.glb", root);
+            }
+            else
+            {
+                Console.WriteLine("No License Available");
+            }
+        }
+    }
+}
---
+++ csharp/Windows/shared/shared_utils/PixyzUtils.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace PiXYZDemo
+{
+    using Newtonsoft.Json;
+    using System.Diagnostics;
+    using UnityEngine.Pixyz.API;
+    using UnityEngine.Pixyz.Geom;
+    using UnityEngine.Pixyz.Scene;
+    using UnityEngine.Pixyz.View;
+
+
+    public enum Orientation
+    {
+        FRONT = 0,
+        BACK = 1,
+        TOP = 2,
+        BOTTOM = 3,
+        LEFT = 4,
+        RIGHT = 5
+    }
+
+    public static class PixyzUtils
+    {
+        public static (double, int, int, int) GetStats(PiXYZAPI api, uint root)
+        {
+            api.Core.ConfigureInterfaceLogger(false, false, false);
+
+            double t = DateTime.Now.TimeOfDay.TotalSeconds;
+            var occurrences = new OccurrenceList((int)root);
+
+            int n_triangles = (int)api.Scene.GetPolygonCount(occurrences, true, false, false);
+            int n_vertices = (int)api.Scene.GetVertexCount(occurrences, false, false, false);
+            int n_parts = api.Scene.GetPartOccurrences((uint)root).length;
+            api.Core.ConfigureInterfaceLogger(true, true, true);
+
+            return (t, n_triangles, n_vertices, n_parts);
+        }
+
+        public static void SaveScreenshot(PiXYZAPI api, int occurrence, string path, Orientation orientation = Orientation.FRONT,
+                                   CameraType type = CameraType.Perspective, int resolution = 1024,
+                                   int fov = 60, bool showEdges = false, bool showLines = false)
+        {
+            Console.WriteLine("Taking Screenshot...");
+
+            Point3 direction = orientation switch
+            {
+                Orientation.FRONT => new Point3 { x = 0, y = 0, z = -1 },
+                Orientation.BACK => new Point3 { x = 0, y = 0, z = 1 },
+                Orientation.TOP => new Point3 { x = 0, y = -1, z = 0 },
+                Orientation.BOTTOM => new Point3 { x = 0, y = 1, z = 0 },
+                Orientation.LEFT => new Point3 { x = 1, y = 0, z = 0 },
+                Orientation.RIGHT => new Point3 { x = -1, y = 0, z = 0 },
+                _ => new Point3 { x = 0, y = 0, z = 0 }
+            };
+
+            var occurrences = new OccurrenceList(occurrence);
+
+            var viewer = api.View.CreateViewer(resolution, resolution);
+            var gpuScene = api.View.CreateGPUScene(occurrences, showEdges);
+            api.View.AddGPUScene(gpuScene, viewer);
+            api.View.FitCamera(direction, type, fov, viewer, occurrences);
+
+            api.View.SetViewerProperty("ShowEdges", showEdges ? "True" : "False", viewer);
+            api.View.SetViewerProperty("ShowLines", showLines ? "True" : "False", viewer);
+
+            api.View.TakeScreenshot(path, viewer);
+            Console.WriteLine("Screenshot saved at: " + path);
+            api.View.DestroyViewer(viewer);
+            api.View.DestroyGPUScene(gpuScene);
+        }
+
+        public static void PrintStats(string fileName, double t, int n_triangles, int _n_triangles,
+                               int n_vertices, int _n_vertices, int n_parts, int _n_parts)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine($"{"file",-20}{fileName}\n");
+            Console.WriteLine($"{"optimization",-20}{t:0.000} s\n");
+            Console.WriteLine($"{"triangles",-20}{n_triangles} -> {_n_triangles}\n");
+            Console.WriteLine($"{"vertices",-20}{n_vertices} -> {_n_vertices}\n");
+            Console.WriteLine($"{"parts",-20}{n_parts} -> {_n_parts}\n");
+        }
+
+        static Dictionary<string, string> GetMetadataDict(MetadataDefinition metadataDefinition)
+        {
+            return new Dictionary<string, string>
+            {
+                { "name", "metadata.Name" },
+                { "value", "metadata.Value" }
+            };
+        }
+
+        static Dictionary<string, object> GetHierarchyDict(PiXYZAPI api, uint occurrence)
+        {
+            var hierarchyDict = new Dictionary<string, object>
+            {
+                { "name", api.Core.GetProperty(occurrence, "Name") }
+            };
+
+            var matrix = api.Scene.GetLocalMatrix(occurrence);
+            var vector3lst = api.Geom.ToTRS(matrix);
+            hierarchyDict.Add("translation", new List<double> { vector3lst[0]._base.x, vector3lst[0]._base.y, vector3lst[0]._base.z });
+            hierarchyDict.Add("rotation", new List<double> { vector3lst[1]._base.x, vector3lst[1]._base.y, vector3lst[1]._base.z });
+            hierarchyDict.Add("scale", new List<double> { vector3lst[2]._base.x, vector3lst[2]._base.y, vector3lst[2]._base.z });
+
+            var children = new List<Dictionary<string, object>>();
+            foreach (var child in api.Scene.GetChildren(occurrence).list)
+            {
+                children.Add(GetHierarchyDict(api, child));
+            }
+            hierarchyDict.Add("children", children);
+
+            if (api.Scene.HasComponent(occurrence, ComponentType.Metadata))
+            {
+                var metadata = api.Scene.GetComponent(occurrence, ComponentType.Metadata);
+
+                var occurrences = new MetadataList((int)metadata);
+
+                var metadataDefinitions = api.Scene.GetMetadatasDefinitions(occurrences);
+
+                var metadataList = new List<Dictionary<string, string>>();
+                foreach (var definition in metadataDefinitions.list)
+                {
+                    metadataList.Add(GetMetadataDict(definition));
+                }
+                //hierarchyDict.Add("metadata", metadataList);
+            }
+
+            return hierarchyDict;
+        }
+
+        public static void ExtractHierarchyToJson(PiXYZAPI api, uint root, string filePath)
+        {
+            var hierarchyDict = GetHierarchyDict(api, root);
+            using (var writer = new StreamWriter(filePath))
+            {
+                var json = JsonConvert.SerializeObject(hierarchyDict, Formatting.Indented);
+                writer.Write(json);
+            }
+            Console.WriteLine("\nDone: structure tree exported at " + filePath);
+        }
+    }
+}
+
